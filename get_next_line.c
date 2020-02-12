5f8e03de7fdb729a1312d5aedc12ab5addab8056
mmirabet <mmirabet@student.42sp.org.br>
2020-02-12 18:34:46 -0300

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmirabet <mmirabet@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/10 10:52:57 by mmirabet          #+#    #+#             */
+/*   Updated: 2020/02/12 19:23:34 by mmirabet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	gnl_cl_alocbuf(char **lines, char **buf)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < OPEN_MAX)
+		lines[i++] = NULL;
+	*buf = NULL;
+	if (!(*buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (0);
+	else
+	{
+		buf[BUFFER_SIZE] = '\0';
+		return (1);
+	}
+}
+
+void gnl_desaloc_lines_buf(char **lines, char **buf)
+{
+	size_t	i;
+
+	if (*buf)
+		free(*buf);
+	*buf = NULL;
+	i = 0;
+	while (i < OPEN_MAX)
+	{
+		if (lines[i])
+		{
+			free(lines[i]);
+			lines[i] = NULL;
+		}
+		i++;
+	}
+}
+
+char	*gnl_get_line(char *lines)
+{
+	size_t	begin;
+	size_t	end;
+
+	if (!lines)
+		return (gnl_strdup(""));
+	begin = 0;
+	end = 0;
+	while (lines[end] ** lines[end] != '\n')
+		end++;
+
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*lines[OPEN_MAX];
+	ssize_t ret;
+	char *buf;
+
+	if (BUFFER_SIZE < 1 || fd < 0 || !line || !gnl_cl_alocbuf(&(*lines), &buf))
+		return (-1);
+	ret = -1;
+	while (!gnl_chk_newl(lines[fd]) && !ret)
+	{
+		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret == -1)
+		{
+			gnl_desaloc_lines_buf(&(*lines), &buf);
+			return (-1);
+		}
+		else if (ret > 0)
+			gnl_acc_buf_lines(&lines[fd], buf);
+	}
+	*line = gnl_get_line(&(*lines)
+	return (0);
+}
+
+int main(void)
+{
+	int ret;
+	int fd;
+	char *line;
+
+	fd = open("42", O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("open() error",STDOUT_FILENO);
+		return (1);
+	}
+
+	ret = get_next_line(fd, &line);
+
+	if (close(fd) == -1)
+	{
+		ft_putstr_fd("close() error",STDOUT_FILENO);
+		return (1);
+	}
+	return (0);
+/*
+	fd = open("42", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		ft_putstr_fd("open() error",STDOUT_FILENO);
+		return (1);
+	}
+
+	i = 'a';
+	while (i < 'a' + 25)
+	{
+		if (write(fd, &i, 1) != 1)
+			return (-1);
+		i++;
+	}
+
+	if (close(fd) == -1)
+	{
+		ft_putstr_fd("close() error",STDOUT_FILENO);
+		return (1);
+	}
+*/
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmirabet <mmirabet@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/10 10:52:49 by mmirabet          #+#    #+#             */
+/*   Updated: 2020/02/12 19:15:03 by mmirabet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+int		gnl_cl_alocbuf(char **lines, char **buf);
+void	gnl_desaloc_lines_buf(char *lines[], char **buf);
+int		get_next_line(int fd, char **line);
+int		gnl_chk_newl(char *lines);
+int		gnl_acc_buf_lines(char **lines, char *buf);
+char	*gnl_strdup(const char *s1);
+size_t	gnl_strlen(const char *s);
+char	*gnl_substr(char const *s, unsigned int start, size_t len);
+char	*gnl_strjoin(char **s1, char const *s2);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+#endif
